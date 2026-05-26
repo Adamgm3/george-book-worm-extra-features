852c432b8cc8ef56e1735f9c99bf06aacc1e2c52
Samuel Bridges <bridsr2@student.op.ac.nz>
2026-05-26 13:10:47 +1200

    Removed trigger for book jump

--- Assets/Scripts/LevelTeleport.cs
+++ Assets/Scripts/LevelTeleport.cs
@@ -5,28 +5,14 @@ using UnityEngine.SceneManagement;
 public class LevelTeleport : MonoBehaviour
 {
  
-    public MeshRenderer sphereMesh;
-    private MeshRenderer teleportCube;
+
     [SerializeField] private string sceneName;
 
-    private void Start()
-    {
-        teleportCube = GetComponent<MeshRenderer>();
-        teleportCube.enabled = false;
-    }
 
-    private void Update()
-    {
-
-        if (sphereMesh.enabled && !teleportCube.enabled)
-        {
-            teleportCube.enabled = true;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && (teleportCube.enabled))
+        if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(sceneName);
         }
