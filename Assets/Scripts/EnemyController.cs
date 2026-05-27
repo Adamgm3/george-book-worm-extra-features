858ca31560fed1e69da8bdb9cdb6bc5a6eab4e58
St1ckyf0x <BIGGMS1@student.op.ac.nz>
2026-05-28 01:28:23 +1200

    a lot of stuff edited, sorted out the pages flying around, enemy behaviour, nav mesh didnt really work well with the way the map was setup, other little changes before building it for design team

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,40 +1,50 @@
-using UnityEditor.Build;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
     bool isCorrupted;
     [SerializeField] GameObject Player;
     Vector3 playerPosition;
     Vector3 enemyPosition;
     Vector3 direction;
-    float speed = 3.5f;
+
+    [SerializeField] float speed = 2f;
+    public float chaseRange = 10f;
+    public float stopDistance = 2f;
+    public int health = 3;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         Move();
 
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
+
     void Move()
     {
         playerPosition = Player.transform.position;
         enemyPosition = transform.position;
+
+        float distance = Vector3.Distance(playerPosition, enemyPosition);
+
+        if (distance > chaseRange || distance < stopDistance)
+            return;
 
         direction = playerPosition - enemyPosition;
         direction.y = 0;
         direction = direction.normalized;
 
         transform.rotation = Quaternion.LookRotation(direction);
-
-        //algorithm
-        transform.position += direction * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -3,41 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] EnemyMovement enemy;
     private InputAction m_attackAction;
     public InputActionAsset inputActions;
     public int damage = 1;
     private bool attackPressed = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     private void Awake()
     {
-        m_attackAction = InputSystem.actions.FindAction("Attack");
+        m_attackAction = inputActions.FindAction("Attack");
     }
 
     private void Update()
     {
         if (m_attackAction.IsPressed())
-        {
             attackPressed = true;
-        }
         else
-        {
             attackPressed = false;
-        }
     }
+
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy") && attackPressed)
         {
-            
-            if (attackPressed == true)
+            EnemyController enemy = other.GetComponent<EnemyController>();
+            if (enemy != null)
             {
                 enemy.health -= damage;
-                Debug.Log("Enemy hit! Remaining Enemy health: " + enemy.health);
+                Debug.Log("Enemy hit! Remaining health: " + enemy.health);
             }
-
         }
-
     }
-
 }
